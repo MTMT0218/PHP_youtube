51b2a540135c776cba3fab14c626b3fd3371cb3f
MOTO <mtmtpkmnxy@gmail.com>
2020-10-02 18:34:40 +0900

    php

--- src/GetVideo.php
+++ src/GetVideo.php
@@ -12,9 +12,8 @@ $videos=array();
 session_start();
 //検索キーワードが入力された場合にapiからデータをもらう
 if (isset($_GET['keyword'])){
-//AIzaSyA0RcnymjK_VzkoWcgEQfyYYrjkXglzSZo
-  //"AIzaSyAWT_BYkZaFQ_iqsq_kaIzaPqBglbM7UHA"
-  $DEVELOPER_KEY="AIzaSyB3RBjEPiTqNHVy90_ZIOgM0mYKxDe3VLA";
+
+  $DEVELOPER_KEY=getenv("YOUTUBE_API");;
   $client = new Google_Client();
   $client->setDeveloperKey($DEVELOPER_KEY);
   $youtube = new Google_Service_YouTube($client);
