3a0f2460d173bf355c33dc8e704db38d41441e32
MOTO <mtmtpkmnxy@gmail.com>
2020-10-04 17:48:46 +0900

    php mysql deploy test

--- src/Video_Table.php
+++ src/Video_Table.php
@@ -4,7 +4,7 @@ public $mysqli;
 
 public function __construct($mysql){
     $this->mysqli=$mysql;
-    var_dump($this->mysqli);
+    var_dump($mysqli);
     //テーブル作成
     $sql = 'CREATE TABLE IF NOT EXISTS video(
         id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
